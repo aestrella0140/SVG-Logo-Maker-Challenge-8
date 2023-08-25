4a923a0b0a72f6ac738848b05d9987f8978a1d4b
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-08-24 18:15:00 -0700

    lots of bug fixes and finishing index.js for rendering and prompt

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 const inquirer = require("inquirer");
 
-const fs = require("fs");
+const { writeFile } = require('fs').promises;
 
-const { Triangle, Circle, Square } = require("./shapes.js");
+const { Triangle, Circle, Square } = require("./lib/shapes.js");
+
+ inquirer
+ .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter text for logo (up to three letters)',
+        },
+        {
+            type: 'list',
+            name: 'shapeSelection',
+            message: 'Choose shape for logo.',
+            choices: ['Triangle', 'Circle', 'Square'],
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'Enter background color via name or Hex color.', 
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter text color via name or Hex color.',
+        }
+    ])
+
+.then((answers) => {
+    let svgInfo;
+    if(answers.shapeSelection ==='Circle') {
+        svgInfo = new Circle();
+        svgInfo.setText(answers.text);
+        svgInfo.setColor(answers.color);
+        svgInfo.setTextColor(answers.textColor);
+    }else if(answers.shapeSelection ==='Triangle') {
+        svgInfo = new Triangle();
+        svgInfo.setText(answers.text);
+        svgInfo.setColor(answers.color);
+        svgInfo.setTextColor(answers.textColor);
+    }else if(answers.shapeSelection ==='Square') {
+        svgInfo = new Square();
+        svgInfo.setText(answers.text);
+        svgInfo.setColor(answers.color);
+        svgInfo.setTextColor(answers.textColor);
+    }
+
+writeFile('./examples/logo.svg', svgInfo.render())
+.then(() => console.log('Successfully created SVG'))
+.catch((err) => console.error(err));
+})
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,31 +1,31 @@
-const { Triangle, Circle, Square } = require("./shapes.js");
+const { Triangle, Circle, Square } = require("../lib/shapes.js");
 // test for triangle given a color blue
 describe("Triangle", () => {
   test("should render background color blue.", () => {
-    const shape = new shape();
+    const shape = new Triangle();
     shape.setColor("blue");
     expect(shape.render()).toEqual(
-      '<polygon points="250,60 100,400 400,400" fill="blue" >'
+      '<polygon points="250,60 100,400 400,400" fill="blue"></polygon>'
     );
   });
 });
 // test for Circle given color green
 describe("Circle", () => {
   test("should render background color green", () => {
-    const shape = new shape();
+    const shape = new Circle();
     shape.setColor("green");
     expect(shape.render()).toEqual(
-      '<circle cx="50" cy="50" r="40" fill="green">'
+      '<circle cx="50" cy="50" r="40" fill="green"></circle>'
     );
   });
 });
 // test for Circle given color red
 describe("Square", () => {
   test("should render background color red", () => {
-    const shape = new shape();
+    const shape = new Square();
     shape.setColor("red");
     expect(shape.render()).toEqual(
-      '<rect x="50" y="20" width="150" height="150" fill="red">'
+      '<rect x="50" y="20" width="150" height="150" fill="red"></rect>'
     );
   });
 });
